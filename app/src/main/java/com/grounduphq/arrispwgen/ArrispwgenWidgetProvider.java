dbe53d4bef736cc93d93a86d372b0919cba6d83b
Raúl Santos <borfast@gmail.com>
2017-01-23 18:19:27 +0000

    Initial code for widget

---
+++ app/src/main/java/com/grounduphq/arrispwgen/ArrispwgenWidgetProvider.java
@@ -0,0 +1,30 @@
+package com.grounduphq.arrispwgen;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+public class ArrispwgenWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int widgetId : appWidgetIds) {
+            String number = "100";
+
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
+                    R.layout.potd_widget);
+            remoteViews.setTextViewText(R.id.textView, number);
+
+            Intent intent = new Intent(context, ArrispwgenWidgetProvider.class);
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+                    0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            remoteViews.setOnClickPendingIntent(R.id.actionButton, pendingIntent);
+            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+        }
+    }
+}
